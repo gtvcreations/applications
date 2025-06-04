1907c8a01a52fb238ef983f49a2ba0c03f730ab3
Thiruvikraman Govindarajan <gtvcreations@gmail.com>
2025-06-04 21:42:29 +0530

    deployment build files through ftp

---
+++ deploy.js
@@ -0,0 +1,48 @@
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+    user: "user",
+    // Password optional, prompted if none given
+    password: "password",
+    host: "ftp.someserver.com",
+    port: 21,
+    localRoot: __dirname + "/local-folder",
+    remoteRoot: "/public_html/remote-folder/",
+    // include: ["*", "**/*"],      // this would upload everything except dot files
+    // include: ["*.php", "dist/*", ".*"],
+    include: ["*", "**/*", ".*"],
+    // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+    // exclude: [
+    //     "dist/**/*.map",
+    //     "node_modules/**",
+    //     "node_modules/**/.*",
+    //     ".git/**",
+    // ],
+    exclude: [],
+    // delete ALL existing files at destination before uploading, if true
+    deleteRemote: false,
+    // Passive mode is forced (EPSV command is not sent)
+    forcePasv: true,
+    // use sftp or ftp
+    sftp: false,
+};
+
+ftpDeploy.deploy(config, function (err, res) {
+    if (err) console.log(err);
+    else console.log("finished:", res);
+});
+ftpDeploy.on("uploading", function (data) {
+    console.log(data.totalFilesCount); // total file count being transferred
+    console.log(data.transferredFileCount); // number of files transferred
+    console.log(data.filename); // partial path with filename being uploaded
+});
+ftpDeploy.on("uploaded", function (data) {
+    console.log(data); // same data as uploading event
+});
+ftpDeploy.on("log", function (data) {
+    console.log(data); // same data as uploading event
+});
+ftpDeploy.on("upload-error", function (data) {
+    console.log(data.err); // data will also include filename, relativePath, and other goodies
+});
